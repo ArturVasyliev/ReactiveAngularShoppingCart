0f386e4084df578f3281fa46766d2a6e4a4e3c2d
Artur Vasyliev <princeartik@gmail.com>
2021-11-09 16:32:23 +0200

    Created shopping-cart and product-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
