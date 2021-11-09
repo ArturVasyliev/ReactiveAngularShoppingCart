26dd5ca59fbff6b5312f395af62cf1a24d0e472d
Artur Vasyliev <princeartik@gmail.com>
2021-11-09 18:06:46 +0200

    Initializing test data and mapping it to the template

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from, Observable, of } from 'rxjs';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'product-list',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products$!: Observable<Product[]>;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.products$ = of([
+      { id: 1, productName: "Lego Star Wars toy", quantity: 1 },
+      { id: 2, productName: "MacBook Air M1", quantity: 1 },
+      { id: 3, productName: "Pepsi", quantity: 1 },
+      { id: 4, productName: "Call of Duty PS5", quantity: 1 },
+      { id: 5, productName: "Spider-Man: No Way Home ticket", quantity: 1 }
+    ]);
   }
 
 }
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'shopping-cart',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
+  products$!: Observable<Product[]>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products$ = of([]);
   }
 
 }
