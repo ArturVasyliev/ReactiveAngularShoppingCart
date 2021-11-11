f7833a2d1466f519100bb21d2577ef5fb4eb3d6c
Artur Vasyliev <princeartik@gmail.com>
2021-11-11 13:20:35 +0200

    Useless attempts to use Marble testing for RxJs

--- src/app/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/shopping-cart/shopping-cart.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ShoppingCartComponent } from './shopping-cart.component';
+import { TestScheduler } from 'rxjs/testing'
+import { removeProducts, ShoppingCartComponent } from './shopping-cart.component';
+import { throttleTime } from 'rxjs/operators';
+import { ShoppingCart } from '../common/shoppingCart.service';
+import { Observable, of } from 'rxjs';
 
 describe('ShoppingCartComponent', () => {
   let component: ShoppingCartComponent;
@@ -21,5 +24,80 @@ describe('ShoppingCartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+});
+
+describe('RemoveProducts Marble Testing', () => {
+  let scheduler: TestScheduler;
+  //let shoppingCart: ShoppingCart;
+  let shoppingCart: jasmine.SpyObj<ShoppingCart>;
+  let product1 = { id: 1, productName: "Lego Star Wars toy", quantity: 1 }
+  let product2 = { id: 4, productName: "Call of Duty PS5", quantity: 1 }
+  let product3 = { id: 3, productName: "Pepsi", quantity: 1 }
+
+  beforeEach(async () => {
+    scheduler = new TestScheduler((received, expected) => {
+      expect(received).toEqual(expected);
+    });
+    shoppingCart = TestBed.inject(ShoppingCart) as jasmine.SpyObj<ShoppingCart>;
+
+    // shoppingCart = new ShoppingCart();
+    // shoppingCart.addProduct(product1);
+    // shoppingCart.addProduct(product1);
+    // shoppingCart.addProduct(product2);
+    // shoppingCart.addProduct(product3);
+  });
+
+  it('should remove products simple', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+
+      spyOn(shoppingCart, 'removeProduct').and.returnValue(of(true));
+
+      // const source = cold('--a--b--a--a--c--c--b--', { a: product1, b: product2, c: product3});
+      // const x = cold('       a-----a--a-----------', { a: product1 });
+      // const y = cold('          b--------------b--', { b: product2 });
+      // const z = cold('                   c--c-----', { c: product3 });
+      // const expect = '     --x--y--------z--------';
+      const source = cold('--a--b--c--', { a: product1, b: product2, c: product3});
+      // const x = cold('       a--------', { a: product1 });
+      // const y = cold('          b-----', { b: product2 });
+      // const z = cold('             c--', { c: product3 });
+      // const x = product1;
+      // const y = product2;
+      // const z = product3;
+      const expect = '     --x--y--z--';
+    
+      const result = source.pipe(removeProducts(shoppingCart));
+
+      expectObservable(result).toBe(expect, { x: true, y: true, z: true });
+    });
+  });
+
+  it('should remove products', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+
+      spyOn(shoppingCart, 'removeProduct').and.returnValue(of(true));
+
+      const source = cold('--a--b--a--a--c--c--b--', { a: product1, b: product2, c: product3});
+      const expect = '     --x--y--x--x--z--z--y--';
+    
+      const result = source.pipe(removeProducts(shoppingCart));
+
+      expectObservable(result).toBe(expect, { x: true, y: true, z: true });
+    });
+  });
+
+  it('should remove products with delay', () => {
+    scheduler.run(({ cold, expectObservable }) => {
+
+      spyOn(shoppingCart, 'removeProduct').and.returnValue(of(true));
+
+      const source = cold('--a--b--a--a--c--c--b--', { a: product1, b: product2, c: product3});
+      const expect = '     --x--y--x--x--z--z--y--';
+    
+      const result = source.pipe(removeProducts(shoppingCart));
+
+      expectObservable(result).toBe(expect, { x: true, y: true, z: true });
+    });
   });
 });
