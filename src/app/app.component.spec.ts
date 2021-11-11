b1527e93d76b57394ec11f59c9cf65876a21e405
Artur Vasyliev <princeartik@gmail.com>
2021-11-11 17:23:02 +0200

    Fixed console warnings when running tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, ShoppingCartComponent, ProductListComponent
       ],
     }).compileComponents();
   });
