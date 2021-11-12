feec10d01c0de73d8cee27d4221d354b1e9ed316
Artur Vasyliev <princeartik@gmail.com>
2021-11-12 10:13:14 +0200

    Refactored tests to include only useful ones

--- src/app/common/shoppingCart.service.spec.ts
+++ src/app/common/shoppingCart.service.spec.ts
@@ -1,17 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
 import { TestScheduler } from 'rxjs/testing';
 import { Product } from '../models/product';
 
-import { ShoppingCart } from './shoppingCart.service';
+import { ShoppingCartService } from './shoppingCart.service';
 
 describe('ShoppingCart', () => {
-  let service: ShoppingCart;
+  let service: ShoppingCartService;
   let scheduler: TestScheduler;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ShoppingCart);
+    service = TestBed.inject(ShoppingCartService);
     service.addProduct({ id: 3, productName: "Pepsi", quantity: 3 });
     scheduler = new TestScheduler((received, expected) => {
       expect(received).toEqual(expected);
@@ -22,64 +21,17 @@ describe('ShoppingCart', () => {
     expect(service).toBeTruthy();
   });
 
-  it('addProduct() on first push returns input product', () => {
-    const product: Product = { id: 1, productName: "Lego Star Wars toy", quantity: 1 };
-    const result = service.addProduct(product);
-
-    scheduler.run(({ expectObservable }) => {
-      const expect = '(a|)';
-      expectObservable(result).toBe(expect, { a: product });
-    });
-  });
-
-  it('addProduct() on duplicate product adding returns updated product', () => {
-    const product: Product = { id: 3, productName: "Pepsi", quantity: 2 };
-    const expectedProduct: Product = { id: 3, productName: "Pepsi", quantity: 5 };
-    const result = service.addProduct(product);
-
-    scheduler.run(({ expectObservable }) => {
-      const expect = '(a|)';
-      expectObservable(result).toBe(expect, { a: expectedProduct });
-    });
-  });
-
   it('addProduct() adds new product to products$ observable', () => {
     const legoProduct: Product = { id: 1, productName: "Lego Star Wars toy", quantity: 1 };
     const pepsiProduct: Product = { id: 3, productName: "Pepsi", quantity: 3 };
     service.addProduct(legoProduct);
-
     service.products$.subscribe(products => expect(products).toEqual([pepsiProduct, legoProduct]));
-
-    // scheduler.run(({ expectObservable }) => {
-    //   const expect = '(a|)';
-    //   expectObservable(service.products$).toBe(expect, { a: pepsiProduct });
-    // });
   });
 
   it('addProduct() updates existing product in products$ observable', () => {
     const expectedPepsiProduct: Product = { id: 3, productName: "Pepsi", quantity: 4 };
     service.addProduct({ id: 3, productName: "Pepsi", quantity: 1 });
     service.products$.subscribe(products => expect(products).toEqual([expectedPepsiProduct]));
-  });
-
-  it('removeProduct() for existing product returns true', () => {
-    const product: Product = { id: 3, productName: "Pepsi", quantity: 1 };
-    const result = service.removeProduct(product);
-
-    scheduler.run(({ expectObservable }) => {
-      const expect = '(a|)';
-      expectObservable(result).toBe(expect, { a: true });
-    });
-  });
-
-  it('removeProduct() for not existing product returns false', () => {
-    const product: Product = { id: 1, productName: "Lego Star Wars toy", quantity: 1 };
-    const result = service.removeProduct(product);
-
-    scheduler.run(({ expectObservable }) => {
-      const expect = '(a|)';
-      expectObservable(result).toBe(expect, { a: false });
-    });
   });
 
   it('removeProduct() removes product from products$ observable', () => {
--- src/app/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/shopping-cart/shopping-cart.component.spec.ts
@@ -1,11 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { TestScheduler } from 'rxjs/testing'
-import { removeProducts, ShoppingCartComponent } from './shopping-cart.component';
-import { throttleTime } from 'rxjs/operators';
-import { ShoppingCart } from '../common/shoppingCart.service';
-import { Observable, of } from 'rxjs';
+import { ShoppingCartComponent } from './shopping-cart.component';
+import { ShoppingCartService } from '../common/shoppingCart.service';
+import { of } from 'rxjs';
 import { By } from '@angular/platform-browser';
-import { Product } from '../models/product';
 
 describe('ShoppingCartComponent', () => {
   let component: ShoppingCartComponent;
@@ -27,7 +24,7 @@ describe('ShoppingCartComponent', () => {
 
     await TestBed.configureTestingModule({
       declarations: [ ShoppingCartComponent ],
-      providers: [ { provide: ShoppingCart, useValue: shoppingCartFake } ],
+      providers: [ { provide: ShoppingCartService, useValue: shoppingCartFake } ],
     })
     .compileComponents();
   });
@@ -47,87 +44,4 @@ describe('ShoppingCartComponent', () => {
     component.products$.subscribe(products => expect(products.length).toEqual(rows.length));
   });
 
-  it('removeProduct() should add product with quantity 1 to the Subject', () => {
-    const inputProduct = { id: 2, productName: "MacBook Air M1", quantity: 2 };
-    const expectedProduct = { id: 2, productName: "MacBook Air M1", quantity: 1 };
-    
-    component['removeStream$'].subscribe(prod => expect(prod).toEqual(expectedProduct));
-    component.removeProduct(inputProduct);
-  });
-
-});
-
-describe('RemoveProducts Marble Testing', () => {
-  let scheduler: TestScheduler;
-  //let shoppingCart: ShoppingCart;
-  let shoppingCart: jasmine.SpyObj<ShoppingCart>;
-  let product1 = { id: 1, productName: "Lego Star Wars toy", quantity: 1 }
-  let product2 = { id: 4, productName: "Call of Duty PS5", quantity: 1 }
-  let product3 = { id: 3, productName: "Pepsi", quantity: 1 }
-
-  beforeEach(async () => {
-    scheduler = new TestScheduler((received, expected) => {
-      expect(received).toEqual(expected);
-    });
-    shoppingCart = TestBed.inject(ShoppingCart) as jasmine.SpyObj<ShoppingCart>;
-
-    // shoppingCart = new ShoppingCart();
-    // shoppingCart.addProduct(product1);
-    // shoppingCart.addProduct(product1);
-    // shoppingCart.addProduct(product2);
-    // shoppingCart.addProduct(product3);
-  });
-
-  it('should remove products simple', () => {
-    scheduler.run(({ cold, expectObservable }) => {
-
-      spyOn(shoppingCart, 'removeProduct').and.returnValue(of(true));
-
-      // const source = cold('--a--b--a--a--c--c--b--', { a: product1, b: product2, c: product3});
-      // const x = cold('       a-----a--a-----------', { a: product1 });
-      // const y = cold('          b--------------b--', { b: product2 });
-      // const z = cold('                   c--c-----', { c: product3 });
-      // const expect = '     --x--y--------z--------';
-      const source = cold('--a--b--c--', { a: product1, b: product2, c: product3});
-      // const x = cold('       a--------', { a: product1 });
-      // const y = cold('          b-----', { b: product2 });
-      // const z = cold('             c--', { c: product3 });
-      // const x = product1;
-      // const y = product2;
-      // const z = product3;
-      const expect = '     --x--y--z--';
-    
-      const result = source.pipe(removeProducts(shoppingCart));
-
-      expectObservable(result).toBe(expect, { x: true, y: true, z: true });
-    });
-  });
-
-  it('should remove products', () => {
-    scheduler.run(({ cold, expectObservable }) => {
-
-      spyOn(shoppingCart, 'removeProduct').and.returnValue(of(true));
-
-      const source = cold('--a--b--a--a--c--c--b--', { a: product1, b: product2, c: product3});
-      const expect = '     --x--y--x--x--z--z--y--';
-    
-      const result = source.pipe(removeProducts(shoppingCart));
-
-      expectObservable(result).toBe(expect, { x: true, y: true, z: true });
-    });
-  });
-
-  it('should remove products with delay', () => {
-    scheduler.run(({ cold, expectObservable }) => {
-
-      spyOn(shoppingCart, 'removeProduct').and.returnValue(of(true));
-
-      const source = cold('--a--b--a--a--c--c--b--', { a: product1, b: product2, c: product3});
-      const expect = '     --x--y--x--x--z--z--y--';
-    
-      const result = source.pipe(removeProducts(shoppingCart));
-
-      expectObservable(result).toBe(expect, { x: true, y: true, z: true });
-    });
-  });
 });
